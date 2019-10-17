9d7f318ee3611d713e38f43303c83195ef41a803
Martin Cornel <mrtncornel@gmail.com>
2019-10-17 14:38:46 +0300

    Shared preference to read and write private files.

--- app/src/main/java/com/mcornel/ecommerce/SecondActivity.kt
+++ app/src/main/java/com/mcornel/ecommerce/SecondActivity.kt
@@ -1,7 +1,8 @@
 package com.mcornel.ecommerce
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
@@ -10,6 +11,22 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-        txtResult.text = intent.getStringExtra("result")
+        var resultString = intent.getStringExtra("result")
+        txtResult.text = resultString
+        btnSave.setOnClickListener {
+            var name = txtName.text.toString()
+            var fileData = "$name: $resultString"
+
+            // using shared preferences to write to file
+            var sharedPref = getSharedPreferences("my_calculations", Context.MODE_PRIVATE)  // creates file
+            var editor = sharedPref.edit()
+            editor.putString("result", fileData)        // add variable result with it's value
+            editor.commit()
+        }
+
+        btnReadFile.setOnClickListener {
+            var sharedPref = getSharedPreferences("my_calculations", Context.MODE_PRIVATE)  // creates file
+            txtRead.text = sharedPref.getString("result", "Nothing found")       // use shared preference to read file or return default
+        }
     }
 }
