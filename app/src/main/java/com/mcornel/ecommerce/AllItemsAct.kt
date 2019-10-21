ed18ef2e5f6fda62205fb053a7951292217a0b46
Martin Cornel <mrtncornel@gmail.com>
2019-10-21 03:17:24 +0300

    Dynamic json object attributes for all items in table list.

--- app/src/main/java/com/mcornel/ecommerce/AllItemsAct.kt
+++ app/src/main/java/com/mcornel/ecommerce/AllItemsAct.kt
@@ -3,11 +3,14 @@ package com.mcornel.ecommerce
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import android.text.method.ScrollingMovementMethod
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_all_items.*
+import org.json.JSONObject
 
 class AllItemsAct : AppCompatActivity() {
 
@@ -20,15 +23,26 @@ class AllItemsAct : AppCompatActivity() {
         val url = "$siteUrl/ecom/get_all.php?table=$table"
         var resultText = ""
 
+        tvTableName.text = table?.toUpperCase()
+
         val requestQ = Volley.newRequestQueue(this)
 
         val jarRequest = JsonArrayRequest(Request.Method.GET, url, null,
             Response.Listener { response ->
-                for (x in 0 until response.length())
-//                    todo figure out how to get dynamic attributes
-                    resultText +=  response.getJSONObject(x).getString("name") + "\n"
+                for (x in 0 until response.length()) {
+                    var jsonObject = response.getJSONObject(x)
 
-                tvResultList.text = resultText
+                    var objectString = "<p>"
+//                    Get values dynamically
+                    for(key in jsonObject.keys()) {
+                        objectString += "<b>$key:</b>  ${jsonObject[key]} <br>"
+                    }
+                    resultText += "$objectString<br></p>"
+                }
+
+//                Utilize html rendering abd make scrollable
+                tvResultList.text = Html.fromHtml(resultText)
+                tvResultList.movementMethod = ScrollingMovementMethod()
             },
             Response.ErrorListener { error ->
                 tvResultList.text = error.message
@@ -36,5 +50,13 @@ class AllItemsAct : AppCompatActivity() {
 
         requestQ.add(jarRequest)
 
+    }
+
+    private fun jsonToMap(json: JSONObject): Map<String, String> {
+        val map: MutableMap<String, String> = linkedMapOf()
+        for (key in json.keys()) {
+            map.put(key, json[key] as String)
+        }
+        return map
     }
 }
